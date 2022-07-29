ff138ee28aefc4e382914bd522a245e7875ef11b
AdrianMayor <adrianmayorsantana@gmail.com>
2022-07-29 17:48:18 +0200

    Starting

---
+++ helpers.js
@@ -0,0 +1,15 @@
+/**
+ * ####################
+ * ## Generate Error ##
+ * ####################
+ */
+
+const generateError = (message, status) => {
+    const err = new Error(message);
+    err.statusCode = status;
+    return err;
+};
+
+module.exports = {
+    generateError,
+};
---
+++ server.js
@@ -0,0 +1,86 @@
+require('dotenv').config();
+
+const express = require('express');
+const fileUpload = require('express-fileupload');
+const morgan = require('morgan');
+const cors = require('cors');
+
+const { PORT, UPLOADS_DIR, MYSQL_HOST } = process.env;
+
+const app = express();
+
+app.use(cors());
+
+app.use(morgan('dev'));
+
+app.use(express.json());
+
+app.use(fileUpload());
+
+app.use(express.static(UPLOADS_DIR));
+
+/**
+ * ########################
+ * ## Endpoints Usuarios ##
+ * ########################
+ */
+
+app.get('/users/:idUser'); // -   Ver el perfil de un usuario y su galeria de fotos.
+
+app.entry('/users'); // -   Registro. -   Extra: Validación por email.
+
+app.entry('/users/login'); // -   Login
+
+app.put('/users'); // -  Editar usuario **TOKEN && ACTIVE**
+
+/**
+ * #####################
+ * ## Endpoints entries ##
+ * #####################
+ */
+app.get('/home'); //  -   Ver ultimas fotos (entries) publicadas por otros usuarios.
+
+app.entry('/entries'); // -   Publicar una foto (con resize) con una descripcion **TOKEN && ACTIVE**
+
+app.get('/entries'); // -   Buscar fotos por texto descriptivo.
+
+app.entry('/entries/:idEntries/like'); // -   Dar / Quitar like a una foto (con autenticación y usuario activo). **TOKEN && ACTIVE**
+app.entry('/entries/:idEntries/comment'); // -   Comentar una foto (con autenticación y usuario activo). **TOKEN && ACTIVE**
+
+/**
+ * ######################
+ * ## Middleware Error ##
+ * ######################
+ */
+
+app.use((err, req, res, next) => {
+    console.error(err);
+
+    res.status(err.statusCode || 500).send({
+        status: 'error',
+        message: err.message,
+    });
+});
+
+/**
+ * ##########################
+ * ## Middleware Not Found ##
+ * ##########################
+ */
+
+app.use((req, res) => {
+    res.status(404).send({
+        status: 'error',
+        message: 'Not found!',
+    });
+});
+
+/**
+ * ######################
+ * ## Server listening ##
+ * ######################
+ */
+
+app.listen(PORT, () => {
+    console.log(`Server listening at http://${MYSQL_HOST}:${PORT}`);
+});
